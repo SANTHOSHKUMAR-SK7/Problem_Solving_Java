d845690517a938df99cb8d2a9af9336dc04fc0e4
SANTHOSHKUMAR-SK7 <santhoshsk7077@gmail.com>
2025-02-26 19:18:57 +0530

    Problem_Solving

--- Reverse_String.java
+++ Reverse_String.java
@@ -1,16 +1,16 @@
-//import java.util.Scanner;
-//
-//public class Reverse_String {
-//    public static void main(String[] args) {
+import java.util.Scanner;
+
+public class Reverse_String {
+    public static void main(String[] args) {
 //
 ////        Method 1
-////        Scanner input = new Scanner(System.in);
-////        System.out.print("Enter the String : ");
-////        char[] c=input.nextLine().toCharArray();
-////        System.out.print("The reversed String is : ");
-////        for(int i=c.length-1;i>=0;i--){
-////            System.out.print(c[i]);
-////        }
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the String : ");
+        char[] c=input.nextLine().toCharArray();
+        System.out.print("The reversed String is : ");
+        for(int i=c.length-1;i>=0;i--){
+            System.out.print(c[i]);
+        }
 //
 //        //Method 2
 //
@@ -49,5 +49,5 @@
 //        }
 //
 //
-//    }
-//}
+    }
+}
