ab408080105f8ce422f4c30d44222a77327104d6
SANTHOSHKUMAR_SK7 <santhoshsk7077@gmail.com>
2025-03-20 05:43:01 +0530

    Problem_Solving

--- Recursion.java
+++ Recursion.java
@@ -1,18 +1,21 @@
 public class Recursion {
     public static void main(String[] args) {
-        msg();
-        System.out.println();
-        msg1();
+        num(5);
+        System.out.println("---------------------------------------------");
+        numRev(5);
     }
-    static void msg(){
-        System.out.println("hai");
-        msg1();
+    static void num(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        num(n-1);
     }
-    static void msg1(){
-        System.out.println("hai1");
-        msg2();
-    }
-    static void msg2(){
-        System.out.println("hai2");
+    static void numRev(int n){
+        if(n==0){
+            return;
+        }
+        numRev(n-1);
+        System.out.println(n);
     }
 }
