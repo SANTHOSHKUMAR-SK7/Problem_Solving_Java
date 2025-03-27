fdd24d063c2aa8cffe0b8a7bad085ac4026c7da9
SANTHOSHKUMAR_SK7 <santhoshsk7077@gmail.com>
2025-03-27 20:00:47 +0530

    Problem_Solving

---
+++ ArrayRotation.java
@@ -0,0 +1,26 @@
+public class ArrayRotation {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        int rot=3;
+        rotate(arr,rot);
+        for (int i =0;i< arr.length;i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+    static void rotate(int[] arr,int rot){
+            int len = arr.length;
+            int k = rot % len;
+            reverse(arr,0,len-1);
+            reverse(arr,0,k-1);
+            reverse(arr,k,len-1);
+    }
+    static void reverse(int[] arr,int left,int right){
+        while(left<right){
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+        }
+    }
+}
