b8409c59b64cc6344151f3212d0d9ad76f2f7d64
SANTHOSHKUMAR_SK7 <santhoshsk7077@gmail.com>
2025-05-10 19:10:08 +0530

    Problem_Solving

---
+++ STACK_and_QUEUE/Queue.java
@@ -0,0 +1,20 @@
+package STACK_and_QUEUE;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+ class Queuee {
+    public static void main(String[] args) {
+        Queue<Integer> queue=new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+        queue.add(6);
+
+        System.out.println(queue.peek()); //Retrieves, but does not remove, the head of this queue, or returns null if this queue is empty.Returns: the head of this queue, or null if this queue is empty
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+    }
+}
---
+++ STACK_and_QUEUE/stack.java
@@ -0,0 +1,21 @@
+package STACK_and_QUEUE;
+
+import java.util.Stack;
+
+public class stack {
+    public static void main(String[] args) {
+        Stack<Integer> stack=new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+//        System.out.println(stack.pop());  when we remove there is nothing in the stack.It shows the error "EmptyStackException"
+        System.out.println(stack);
+    }
+}
