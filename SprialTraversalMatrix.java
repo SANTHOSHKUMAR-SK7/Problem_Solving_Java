9f57f1a9dada426e7d3ff789dbd9fffd704c3357
SANTHOSHKUMAR_SK7 <santhoshsk7077@gmail.com>
2025-05-11 22:27:49 +0530

    Problem_Solving

--- SprialTraversalMatrix.java
+++ SprialTraversalMatrix.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 public class SprialTraversalMatrix {
     public static void main(String[] args) {
         int[][] matrix = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
+                {1, 2, 3 ,4},
+                {5, 6, 7 ,8},
+                {9, 10, 11, 12}
         };
         int RowLow=0;
         int RowUp= matrix.length-1;
